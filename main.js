f97bbcb07a62cb043c971cff09b9fe6cb11d75fe
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2024-03-11 14:51:48 +0100

    Chore : adding javascript file

---
+++ main.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // let openButton = document.getElementById("burgermenu-openbutton");
+    // let closeButton = document.getElementById("burgermenu-closebutton");
+    // let burgermenu = document.getElementById("burgermenu-content");
+
+    // // Ouvre le menu burger en rajoutant la class "active"
+    // openButton.addEventListener("click", function() {
+    //     burgermenu.classList.add('active');
+    // });
+
+    // closeButton.addEventListener("click", function() {
+    //     burgermenu.classList.remove('active');
+    // });
+});
